a18897c9756a310b660ffccfc128e101e4331534
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-11-04 15:05:55 -0300

    4.0 Implementando Comportamentos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,45 +4,101 @@ fun main() {
     val contaAlex: Conta = Conta();
     contaAlex.titular = "Alex";
     contaAlex.numero = 1000;
-    contaAlex.saldo = 200.0;
+    contaAlex.setSaldo(-200.0);
+    println(contaAlex.getSaldo());
 
     val contaFran: Conta = Conta();
     contaFran.titular = "Fran";
     contaFran.numero = 1001;
-    contaFran.saldo = 300.0;
+    contaFran.setSaldo(300.0);
+    println(contaFran.getSaldo());
 
-    println(contaAlex.titular);
+
+    /*println(contaAlex.titular);
     println(contaAlex.numero);
     println(contaAlex.saldo);
     println(contaFran.titular);
     println(contaFran.numero);
-    println(contaFran.saldo);
-    println("--------")
-    println(contaAlex.saldo);
-    println("Depositando na conta do Alex");
-    deposita(contaAlex, 50.0);
-    println(contaAlex.saldo);
-
-    println(contaFran.saldo);
-    println("Depositando na conta da Fran");
-    deposita(contaFran, 70.0);
-    println(contaFran.saldo);
+    println(contaFran.saldo);*/
+//    /*println("--------")
+//    println(contaAlex.saldo);
+//    println("Depositando na conta do Alex");
+//    contaAlex.deposita(50.0);
+//    println(contaAlex.saldo);
+//    println(contaFran.saldo);
+//    println("Depositando na conta da Fran");
+//    contaFran.deposita(70.0);
+//    println(contaFran.saldo);
+//
+//    println("--------")
+//    println("--------")
+//    println("Sacando na conta do Alex");
+//    contaAlex.saca(250.0);
+//    println(contaAlex.saldo);
+//    println("Depositando na conta da Fran");
+//    contaFran.saca(100.0);
+//    println(contaFran.saldo);
+//    println("--------")
+//    println("--------")
+//    println("Saque em excesso conta Alex");
+//    contaAlex.saca(100.0);
+//    println(contaAlex.saldo);
+//    println("Saque em excesso conta Fran");
+//    contaAlex.saca(500.0);
+//    println(contaFran.saldo);
+//    println("--------")
+//    println("--------")
+//
+//    println(contaFran.saldo);
+//    println(contaAlex.saldo);
+//    println("Transferência Fran -> Alex");
+//    if(contaFran.transfere(100.0, contaAlex)) println("Transferência sucedida!");
+//    else println("Falha na transferência.")
+//    println(contaFran.saldo);
+//    println(contaAlex.saldo);
+//    println("---")
+//    println(contaFran.saldo);
+//    println(contaAlex.saldo);
+//    println("Transferência Alex -> Fran");
+//    if(contaAlex.transfere(300.0, contaFran)) println("Transferência sucedida!");
+//    else println("Falha na transferência.")
+//    println(contaFran.saldo);
+//    println(contaAlex.saldo);*/
 
 
 }
 
-fun deposita(conta: Conta, valor: Double) {
-    conta.saldo += valor;
-}
 
 class Conta {
     var titular: String = "";
     var numero: Int = 0;
-    var saldo: Double = 0.0;
+    private var saldo: Double = 0.0;
 
-    /*fun deposita(valor: Double){
+    fun deposita(valor: Double) {
         this.saldo += valor;
-    }*/
+    }
+
+    fun saca(valor: Double) {
+        if (this.saldo >= valor) this.saldo -= valor;
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
+
+    fun getSaldo(): Double {
+        return this.saldo;
+    }
+
+    fun setSaldo(valor: Double) {
+        if(valor > 0) this.saldo = valor;
+    }
+
 }
 
 fun testaCopiasEReferencias() {
