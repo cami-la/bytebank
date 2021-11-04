ffc7db81c31c3a3320f7f37db48067c3453b7ef8
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-11-04 11:26:37 -0300

    1.0 Primeiros passos

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,14 @@
 fun main(){
     println("Bem-vindo ao Bytebank");
-    val titular  = "Alex";
+    val titular:String  = "Alex";
+    val numeroConta:Int = 1000;
+    var saldo:Double = 0.0;
 
-    print("titular $titular");
+    saldo = 100 + 2.0;
+    saldo += 200;
+
+    println("titular $titular");
+    println("Numero da conta $numeroConta");
+    println("saldo da conta $saldo");
 
 }
