89d4ddd808106d414b67a66da038811b00b72a57
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-11-06 18:23:05 -0300

    03. Variáveis globais e objects

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,21 +1,35 @@
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.*
+
+
 
 fun main() {
     /*val palavra: String = "";
     val palavraJava: StringJava = StringJava("");*/
 
-    var totalContas: Int = 0;
+    val fran = object : Autenticavel {
+        val nome: String = "Fran";
+        val cpf: String = "111.111.111-..";
+        val senha: Int = 10000
 
-    val alex= Cliente(
+        override fun autentica(senha: Int) = this.senha == senha;
+    }
+
+    val sistemaInterno = SistemaInterno();
+    sistemaInterno.entra(autenticavel =fran, senha= 10000);
+
+    println("Nome do cliente ${fran.nome}");
+
+    val alex = Cliente(
         nome = "Alex",
         cpf = "111.111.111-11",
-        senha = 1);
-    totalContas++;
+        senha = 1
+    );
+    val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000);
+    val contaCorrente = ContaCorrente(titular = alex, numero = 1001);
 
     testaContasDiferentes();
 
-    println("Total de contas: ${totalContas}");
+    println("Total de contas: ${Conta.total}");
 }
 
 
--- src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/Conta.kt
@@ -7,6 +7,14 @@ abstract class Conta(
     var saldo: Double = 0.0
         protected set;
 
+    companion object {
+        var total = 0
+            private set;
+    }
+
+    init {
+        total++;
+    }
 
     fun deposita(valor: Double) {
         if(valor > 0) this.saldo += valor;
@@ -23,4 +31,32 @@ abstract class Conta(
         return false;
     }
 
+}
+
+class ContaCorrente(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+
+    override fun saca(valor: Double) {
+        val valorComTaxa = valor + 0.1;
+        if(this.saldo >= valorComTaxa) this.saldo -= valorComTaxa;
+    }
+
+}
+
+class ContaPoupanca(
+    titular: Cliente,
+    numero: Int
+) : Conta(
+    titular = titular,
+    numero = numero
+) {
+    override fun saca(valor: Double) {
+        if (this.saldo >= valor)
+            this.saldo -= this.saldo - valor;
+    }
 }
