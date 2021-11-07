14cd60e3bf4738b06922f063e16667fbf94e8b26
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-11-07 19:27:24 -0300

    02. Tratando Exceptions

--- src/main/kotlin/br/com/alura/bytebank/main.kt
+++ src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -3,19 +3,51 @@ import br.com.alura.bytebank.test.testaAny
 
 fun main() {
     println("início main")
+    
+    val entrada: String = "1.0";
+
+    val valorRecebido: Double? = try{
+        entrada.toDouble();
+    }catch(e: NumberFormatException) {
+        println("Problema na conversão");
+        e.printStackTrace();
+        null;
+    }
+
+    if(valorRecebido != null){
+        println("valor recebido: $valorRecebido");
+    } else{
+        println("valor inválido")
+    }
+
+    val valorComTaxa: Double? = if(valorRecebido !=null){
+        valorRecebido+0.1;
+    }else{null}
+
+    if(valorComTaxa != null){
+        println("valor recebido: $valorComTaxa);
+    } else{
+        println("valor inválido");
+    }
+
     funcao1()
     println("fim main")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("início funcao1")
-    funcao2()
+    try{
+        funcao2()
+    }catch (e: ClassCastException){
+        e.printStackTrace()
+        println("ClassCastException foi pegada.")
+    }
     println("fim funcao1")
 }
 
 fun funcao2() {
     println("início funcao2")
-    for (i in 1..5){
+    for(i in 1..5){
         println(i)
         val endereco: Any = Any();
         endereco as Endereco;
