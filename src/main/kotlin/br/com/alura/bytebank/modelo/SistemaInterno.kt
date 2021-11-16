8fdd1a27141157298c2d445dd44d26b2299f5de8
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-11-16 13:50:09 -0300

    05. Implementando HOF com receivers

--- src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/main/kotlin/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -10,4 +10,16 @@ class SistemaInterno {
         else println("Falha na autenticação")
     }
 
+    fun entraReceiver(autenticavel: Autenticavel, senha: Int, autenticado: SistemaInterno.() -> Unit = {}){
+        if(autenticavel.autentica(senha)) {
+            println("Bem-vindo ao Bytebank")
+            autenticado()
+        };
+        else println("Falha na autenticação")
+    }
+
+    fun pagamento(){
+        print("Realizando pagamento")
+    }
+
 }
